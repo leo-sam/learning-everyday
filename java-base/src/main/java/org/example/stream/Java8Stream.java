73cdf79c3120403ccf3597f79cddda5f69977686
huangshengyao <huangshengyao@jiandanxinli.com>
2021-11-15 18:19:38 +0800

    feat(Java8Stream): add stream function demo
    2021-11-15
    
    1. streamListToString
    2. streamListToMap

--- java-base/src/main/java/org/example/stream/Java8Stream.java
+++ java-base/src/main/java/org/example/stream/Java8Stream.java
@@ -1,4 +1,69 @@
 package org.example.stream;
 
+import lombok.Data;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+/**
+ * @author huangsy
+ */
 public class Java8Stream {
+
+    public static void main(String[] args) {
+        streamListToString();
+        streamListToMap();
+    }
+
+    public static void streamListToString() {
+        List<String> list1 = Arrays.asList("文学","小说","历史","言情","科幻","悬疑");
+
+        List<String> list2 = Arrays.asList("文学","小说","历史","言情","科幻","悬疑");
+
+        //方案一：使用String.join()函数，给函数传递一个分隔符合一个迭代器，一个StringJoiner对象会帮助我们完成所有的事情
+        String string1 = String.join("-",list1);
+        System.out.println("string1 = " + string1);
+        //方案二：采用流的方式来写
+        String string2 = list2.stream().collect(Collectors.joining(","));
+        System.out.println("string2 = " + string2);
+    }
+
+    public static void streamListToMap() {
+        User u1 = new User("1", true, "a");
+        User u2 = new User("2", false, "b");
+        User u3 = new User("3", false, "c");
+        List<User> userList = Arrays.asList(u1, u2, u3);
+
+        Map<String, User> userMap = userList.stream().collect(Collectors.toMap(User::getName, v -> v));
+        System.out.println("userMap = " + userMap);
+
+        // 排除bean为空
+        User u4 = null;
+        List<User> userList2 = Arrays.asList(u1, u2, u3, u4);
+        Map<String, User> userMapWithoutNull = userList2.stream().filter(Objects::nonNull)
+                .collect(Collectors.toMap(User::getName, v -> v));
+        System.out.println("userMapWithoutNull = " + userMapWithoutNull);
+    }
+
+    @Data
+    public static class User {
+        private String about;
+
+        private boolean lock;
+
+        private String name;
+
+        public User() {
+        }
+
+        public User(String about, boolean lock, String name) {
+            super();
+            this.about = about;
+            this.lock = lock;
+            this.name = name;
+        }
+    }
 }
