b1b3560fc89411ce107a1cbf6a1486b4c6758bee
leo-sam <lever0066@gmail.com>
2021-11-16 19:01:04 +0800

    feat(Java8Stream): add stream Collector demo
    2021-11-16
    
    1. streamListToString
    2. streamListToMap
    3. removeDuplicationByStream
    4. streamListCollectors

--- java-base/src/main/java/org/example/stream/Java8Stream.java
+++ java-base/src/main/java/org/example/stream/Java8Stream.java
@@ -2,10 +2,7 @@ package org.example.stream;
 
 import lombok.Data;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -17,6 +14,8 @@ public class Java8Stream {
         streamListToString();
         streamListToMap();
         removeDuplicationByStream();
+        streamListCompare();
+        streamListCollectors();
     }
 
     /**
@@ -39,9 +38,9 @@ public class Java8Stream {
      * 把List转化成Map
      */
     public static void streamListToMap() {
-        User u1 = new User("1", true, "a");
-        User u2 = new User("2", false, "b");
-        User u3 = new User("3", false, "c");
+        User u1 = new User(1, true, "a");
+        User u2 = new User(2, false, "b");
+        User u3 = new User(3, false, "c");
         List<User> userList = Arrays.asList(u1, u2, u3);
 
         Map<String, User> userMap = userList.stream().collect(Collectors.toMap(User::getName, v -> v));
@@ -56,11 +55,94 @@ public class Java8Stream {
     }
 
     /**
+     * 根据List中的某一个字段进行大小比较
+     */
+    public static void streamListCompare() {
+        User u1 = new User(1, true, "a");
+        User u2 = new User(2, false, "b");
+        User u3 = new User(3, false, "c");
+        List<User> userList = Arrays.asList(u1, u2, u3);
+
+        // max
+        userList.stream().map(User::getId).max(Comparator.comparing(Integer::intValue))
+                .ifPresent(e -> System.out.println("Max: " + e));
+        // min
+        userList.stream().map(User::getId).min(Comparator.comparing(Integer::intValue))
+                .ifPresent(e -> System.out.println("Min: " + e));
+    }
+
+    /**
+     * Collector 工具库：Collectors
+     * 感谢原作者:旅行者yky
+     * 地址:https://blog.csdn.net/y_k_y/article/details/84633001
+     */
+    public static void streamListCollectors() {
+        Student s1 = new Student("aa", 10,1);
+        Student s2 = new Student("bb", 20,2);
+        Student s3 = new Student("cc", 10,3);
+        List<Student> list = Arrays.asList(s1, s2, s3);
+
+        //装成list
+        // [10, 20, 10]
+        List<Integer> ageList = list.stream().map(Student::getAge).collect(Collectors.toList());
+        System.out.println("ageList = " + ageList);
+
+        //转成set
+        // [20, 10]
+        Set<Integer> ageSet = list.stream().map(Student::getAge).collect(Collectors.toSet());
+        System.out.println("ageSet = " + ageSet);
+
+        //转成map,注:key不能相同，否则报错
+        // {cc=10, bb=20, aa=10}
+        Map<String, Integer> studentMap = list.stream().collect(Collectors.toMap(Student::getName, Student::getAge));
+        System.out.println("studentMap = " + studentMap);
+
+        //字符串分隔符连接
+        // (aa,bb,cc)
+        String joinName = list.stream().map(Student::getName).collect(Collectors.joining(",", "(", ")"));
+        System.out.println("joinName = " + joinName);
+
+        //聚合操作
+        //1.学生年龄小于20的数量
+        // 2
+        long count = list.stream().filter(s -> s.age < 20).count();
+        //2.最大年龄 (最小的minBy同理)
+        // 20
+        Integer maxAge = list.stream().map(Student::getAge).max(Integer::compare).get();
+        //3.所有人的年龄
+        // 40
+        int sumAge = list.stream().mapToInt(Student::getAge).sum();
+        //4.平均年龄
+        // 13.333333333333334
+        Double averageAge = list.stream().collect(Collectors.averagingDouble(Student::getAge));
+        System.out.println("count:" + count + ",max:" + maxAge + ",sum:" + sumAge + ",average:" + averageAge);
+
+        // 带上以上所有方法
+        DoubleSummaryStatistics statistics = list.stream().collect(Collectors.summarizingDouble(Student::getAge));
+        System.out.println("DoubleSummaryStatistics count:" + statistics.getCount() + ",max:" + statistics.getMax() + ",sum:" + statistics.getSum() + ",average:" + statistics.getAverage());
+
+        //分组
+        Map<Integer, List<Student>> ageMap = list.stream().collect(Collectors.groupingBy(Student::getAge));
+        System.out.println("ageMap = " + ageMap);
+        //多重分组,先根据类型分再根据年龄分
+        Map<Integer, Map<Integer, List<Student>>> typeAgeMap = list.stream().collect(Collectors.groupingBy(Student::getType, Collectors.groupingBy(Student::getAge)));
+        System.out.println("typeAgeMap = " + typeAgeMap);
+        //分区
+        //分成两部分，一部分大于10岁，一部分小于等于10岁
+        Map<Boolean, List<Student>> partMap = list.stream().collect(Collectors.partitioningBy(v -> v.getAge() > 10));
+        System.out.println("partMap = " + partMap);
+        //规约
+        //40
+        Integer allAge = list.stream().map(Student::getAge).reduce(Integer::sum).get();
+        System.out.println("allAge = " + allAge);
+    }
+
+    /**
      * 使用java8新特性stream实现List去重(有序)
      */
     public static void removeDuplicationByStream() {
-        User u1 = new User("1", true, "a");
-        User u2 = new User("2", false, "b");
+        User u1 = new User(1, true, "a");
+        User u2 = new User(2, false, "b");
         List<User> userList = Arrays.asList(u1, u2, u2);
         List<User> newList = userList.stream().distinct().collect(Collectors.toList());
         System.out.println("newList = " + newList);
@@ -70,7 +152,7 @@ public class Java8Stream {
      */
     @Data
     public static class User {
-        private String about;
+        private int id;
 
         private boolean lock;
 
@@ -79,10 +161,27 @@ public class Java8Stream {
         public User() {
         }
 
-        public User(String about, boolean lock, String name) {
+        public User(int id, boolean lock, String name) {
             super();
-            this.about = about;
+            this.id = id;
             this.lock = lock;
+            this.name = name;
+        }
+    }
+    @Data
+    public static class Student {
+
+        private String name;
+        private int age;
+        private int type;
+
+        public Student() {
+        }
+
+        public Student(String name, int age, int type) {
+            super();
+            this.age = age;
+            this.type = type;
             this.name = name;
         }
     }
